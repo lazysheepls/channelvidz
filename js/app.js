e6c98a7b55c06972b2fcb3f3beed1739423a5e88
lazysheepls <lazysheep.white@gmail.com>
2016-01-28 00:59:46 +0800

    app finish

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ $(document).ready(function(){
 });
 
 function onDeviceReady(){
-	
+	// localStorage.setItem('maxResults',10);
 	//Check local storage channel
-	if(localStorage.channel==null || localStorage.channel==''){
-		console.log("Enter Here");
+	if(localStorage.channel == null || localStorage.channel == ''){
+		// Ask User for Channel
 		$('#popupDialog').popup("open");
-	}else{
-		var channel=localStorage.getItem('channel');
+	} else {
+		var channel = localStorage.getItem('channel');
 	}
 	// var channel = 'TechGuyWeb';
 
@@ -18,6 +18,30 @@ function onDeviceReady(){
 	// Handle Cilck event
 	$(document).on('click','#vidlist li',function(){
 		showVideo($(this).attr('videoId'));
+	});
+
+	$('#channelBtnOk').click(function(){
+		var channel = $('#channelName').val();
+		setChannel(channel);
+		getPlaylist(channel);
+	});
+
+	//Option page
+	$('#saveOptions').click(function(){
+		saveOptions();
+	});
+
+	$('#clearChannel').click(function(){
+		clearChannel();
+	});
+
+	$(document).on('pageinit','#options',function(e){
+		console.log("Hre");
+		var channel=localStorage.getItem('channel');
+		var maxResults=localStorage.getItem('maxResults');
+
+		$('#channelNameOptions').attr('value',channel);
+		$('#maxResultsOptions').attr('value',maxResults);
 	});
 }
 
@@ -37,7 +61,7 @@ function getPlaylist (channel) {
 				// console.log(data);
 				$.each(data.items, function(i, item){
 					playlistId = item.contentDetails.relatedPlaylists.uploads;
-					getVideos(playlistId,10);
+					getVideos(playlistId,localStorage.getItem('maxResults'));
 				})
 			}
 		);
@@ -72,4 +96,34 @@ function showVideo(id){
 	var output='<iframe width="100%" height="250" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe>';
 	// PUT in the show video section in to the one-page-app
 	$('#showvideo').html(output);
+}
+
+function setChannel(channel){
+	localStorage.setItem('channel',channel);
+}
+
+function setMaxResults(maxResults){
+	localStorage.setItem('maxResults',maxResults);
+}
+
+function saveOptions(){
+	var channel=$('#channelNameOptions').val();
+	setChannel(channel);
+	var maxResults=$('#maxResultsOptions').val();
+	setMaxResults(maxResults);
+	//Refresh the page is necessary
+	$('body').pagecontainer('change','#main',{options});
+	//Need to regenerate the list
+	getPlaylist(channel);
+}
+
+
+function clearChannel(){
+	localStorage.removeItem('channel');
+	//Refresh the page is necessary
+	$('body').pagecontainer('change','#main',{options});
+	//Need to empty the list
+	$('#vidlist').html('');
+	//Reopen the pop up
+	$('#popupDialog').popup('open');
 }
